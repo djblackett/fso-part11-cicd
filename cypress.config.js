0f9f226d5eec42de5f9eddc87b8115df30c45c18
djblackett <49618491+djblackett@users.noreply.github.com>
2023-03-16 17:14:27 -0300

    add config options to cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+      baseUrl: 'http://localhost:5000',
+      specPattern: 'e2e/*.cy.js',
+      supportFile: false,
+      pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
