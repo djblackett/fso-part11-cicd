192972bdf3d9eeea25e546e0f4bc4046f48ef014
djblackett <49618491+djblackett@users.noreply.github.com>
2023-03-16 17:00:29 -0300

    add '/' to end of URLs in cypress tests

--- cypress/e2e/pokemon-tests.cy.js
+++ cypress/e2e/pokemon-tests.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:5000/')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
@@ -8,7 +8,7 @@ describe('Pokedex', function() {
 
   // eslint-disable-next-line
   it("Clicking a pokemon loads that pokemon's page", function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:5000/')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
     cy.contains('overgrow')
