e0ffb5e82ba3a8d74b0fdb86a20e92a77485e2a7
djblackett <49618491+djblackett@users.noreply.github.com>
2023-03-16 18:23:48 -0300

    fix cypress spec location configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-      baseUrl: 'http://localhost:5000',
-      specPattern: 'e2e/*.cy.js',
+      baseUrl: 'http://localhost:5000/',
+      specPattern: 'cypress/e2e/*.cy.js',
       supportFile: false,
       pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
